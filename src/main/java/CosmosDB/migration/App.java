5850601f681429eb330b7e078657d601d63eabec
Saumya Khandelwal <saumya.khandelwal@sap.com>
2020-07-30 16:53:24 +0530

    Adding the project

---
+++ src/main/java/CosmosDB/migration/App.java
@@ -0,0 +1,13 @@
+package CosmosDB.migration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
